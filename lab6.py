76b6fb1cad789636a3a4560f11154a1696d31b6d
djwilson419 <149012970+djwilson419@users.noreply.github.com>
2023-10-30 15:56:19 -0400

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,37 @@
+def encoder(message):
+    result = ''
+    for digit in message:
+        new_digit = str((int(digit) + 3) % 10)
+        result += new_digit
+    return result
+
+
+def decoder(message):
+    result = ''
+    for digit in message:
+        new_digit = str((int(digit) - 3) % 10)
+        result += new_digit
+    return result
+
+
+def main():
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        choice = input("Please enter an option: ")
+        if choice == "1":
+            value = input("Please enter your password to encode: ")
+            print("Your password has been encoded and stored!")
+        elif choice == "2":
+            print("The encoded password is ", encoder(value), "and the original password is ", value)
+        elif choice == "3":
+            break
+        else:
+            print("Invalid choice")
+        print()
+
+
+main()
